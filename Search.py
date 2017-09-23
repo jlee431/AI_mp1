d9fa7c10baccb8c851cc961c6afd7ea38cf4a39e
splinga2 <splinga2@illinois.edu>
2017-09-23 14:41:53 -0500

    Updated search with DFS

--- Search.py
+++ Search.py
@@ -3,12 +3,9 @@ import sys
 if(len(sys.argv) !=2):
 	print("USAGE: Search.py mazefile")
 
+
+
 class State:
-	x_pos = 0
-	y_pos = 0
-	dots_left = 0
-	eaten_list = []
-	parent = None
 
 	def __init__(self, x = 0, y = 0, d = 0, el = [], p = None):
 		self.x_pos = x
@@ -17,10 +14,20 @@ class State:
 		self.eaten_list = el
 		self.parent = p
 
-mazeFile = open(sys.argv[1], 'r')
+	def toString(self):
+		s = str(self.x_pos) + str(self.y_pos)
+		for e in self.eaten_list:
+			if(e):
+				s = s + '1'
+			else:
+				s = s + '0'
+		return s
+
+
+maze_file = open(sys.argv[1], 'r')
 
 maze = []
-for line in mazeFile:
+for line in maze_file:
 	maze.append(list(line)[:len(line)-1])
 
 for line in maze:
@@ -28,6 +35,87 @@ for line in maze:
 		sys.stdout.write(c)
 	print('')
 
-second = State(4)
-first = State(0,0,0,0,second)
-print(first.parent.x_pos)
+dots = []
+player_start_x = 0
+player_start_y = 0
+
+for row in range(len(maze)):
+	for col in range(len(maze[row])):
+		if(maze[row][col] == '.'):
+			dots.append((col, row)) 
+		elif(maze[row][col] == 'P'):
+			player_start_y = row
+			player_start_x = col
+
+print('Player pos: ' + str(player_start_x) + ', ' + str(player_start_y))
+print('Dot pos: ' + str(dots[0][0]) + ', ' + str(dots[0][1]))
+
+
+frontier = []
+prev_states = {}
+
+# Add initial state
+
+frontier.append(State(player_start_x, player_start_y, len(dots), [False]*len(dots)))
+
+# Start Search
+
+while frontier:
+	# Get the next state
+	current_state = frontier.pop()
+
+	# Check if current state is goal state
+	if(not current_state.dots_left):
+		while current_state:
+			maze[current_state.y_pos][current_state.x_pos] = '.'
+			current_state = current_state.parent
+		break
+
+	# Check for repeated state
+	if(current_state.toString() in prev_states):
+		continue
+
+	# Expand current state
+	prev_states[current_state.toString()] = True
+
+	# Check all four directions
+	char = maze[current_state.y_pos-1][current_state.x_pos]
+	if(char == ' '):
+		frontier.append(State(current_state.x_pos, current_state.y_pos - 1, current_state.dots_left, list(current_state.eaten_list), current_state))
+	elif(char == '.'):
+		l = list(current_state.eaten_list)
+		i = dots.index((current_state.x_pos, current_state.y_pos - 1))
+		l[i] = True
+		frontier.append(State(current_state.x_pos, current_state.y_pos - 1, current_state.dots_left - 1, l, current_state))
+
+	char = maze[current_state.y_pos][current_state.x_pos + 1]
+	if(char == ' '):
+		frontier.append(State(current_state.x_pos + 1, current_state.y_pos, current_state.dots_left, list(current_state.eaten_list), current_state))
+	elif(char == '.'):
+		l = list(current_state.eaten_list)
+		i = dots.index((current_state.x_pos + 1, current_state.y_pos))
+		l[i] = True
+		frontier.append(State(current_state.x_pos + 1, current_state.y_pos, current_state.dots_left - 1, l, current_state))
+
+	char = maze[current_state.y_pos + 1][current_state.x_pos]
+	if(char == ' '):
+		frontier.append(State(current_state.x_pos, current_state.y_pos + 1, current_state.dots_left, list(current_state.eaten_list), current_state))
+	elif(char == '.'):
+		l = list(current_state.eaten_list)
+		i = dots.index((current_state.x_pos, current_state.y_pos + 1))
+		l[i] = True
+		frontier.append(State(current_state.x_pos, current_state.y_pos + 1, current_state.dots_left - 1, l, current_state))
+
+	char = maze[current_state.y_pos][current_state.x_pos - 1]
+	if(char == ' '):
+		frontier.append(State(current_state.x_pos - 1, current_state.y_pos, current_state.dots_left, list(current_state.eaten_list), current_state))
+	elif(char == '.'):
+		l = list(current_state.eaten_list)
+		i = dots.index((current_state.x_pos - 1, current_state.y_pos))
+		l[i] = True
+		frontier.append(State(current_state.x_pos - 1, current_state.y_pos, current_state.dots_left - 1, l, current_state))
+
+for line in maze:
+	for c in line:
+		sys.stdout.write(c)
+	print('')
